c8c6dab7dfa0c4f7291b6c2c97321220979f9a05
OleMan <aom-95@live.com>
2018-03-30 00:47:35 +0300

    #4.1: scss and few pages

---
+++ src/components/login/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Header from '../partials/Header';
+
+class Experts extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			experts:[]
+		};
+	}
+
+	async componentDidMount() {
+		const res = await fetch('/v1/experts');
+		console.log(res);
+
+		if(res.status === 200){
+			this.setState({
+				experts: await res.json()
+			});
+		}
+	}
+
+	render() {
+		return (
+			<div className="experts-page">
+        <Header />
+        <div className="">
+	        <p>Experts</p>
+	        {
+		        this.state.experts.map(function(expert, i){
+			        console.log('test');
+			        return <li key={i}>{expert.name}</li>
+		        })
+	        }
+        </div>
+        <footer className="App-footer">
+          <p>Authors:</p>
+          <a href="/">Oleksii Manachynskyi</a>
+          <a href="/">Roman Suprun</a>
+        </footer>
+			</div>
+		);
+	}
+}
+
+export default Experts;
---
+++ src/components/partials/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from "./logo.svg";
+import './Header.scss';
+
+class Header extends Component {
+
+	render() {
+		return (
+      <header className="header">
+	      <img src={logo} alt={'logo'}/>
+	      <h1 className="title">GIMET CMS</h1>
+      </header>
+		);
+	}
+}
+
+export default Header;
